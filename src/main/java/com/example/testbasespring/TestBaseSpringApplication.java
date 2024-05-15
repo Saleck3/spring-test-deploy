2734abed8f58f3c96782ee63250f63b7a87833ec
Saleck3 <aledagonale@gmail.com>
2024-05-15 00:10:13 -0300

    added all the code this app needs

---
+++ src/main/java/com/example/testbasespring/TestBaseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbasespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBaseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBaseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testbasespring/TestBaseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbasespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBaseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
